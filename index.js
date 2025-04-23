fe13a242977e803fae1b2841e67c1ecc3fb0d5f9
Saujanya Das <dassaujanya1911@gmail.com>
2025-04-23 13:21:08 +0530

    changes

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  connectTimeout: 10000,
 });
 
 // Test database connection on startup
